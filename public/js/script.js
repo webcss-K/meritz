0a85aa414a91a57439444564a7312dbe5896ab10
webcss-K <webcss@fluidinc.co.kr>
2025-03-26 17:51:12 +0900

    3/26 수정

--- public/js/script.js
+++ public/js/script.js
@@ -1,52 +1,4 @@
 $(document).ready(function(){
-    const swiper_member = new Swiper('.section-visual .member-list', {
-        slidesPerView: 1,
-        spaceBetween: 15,
-        direction: 'vertical',
-        loop : true,
-        loopAdditionalSlides : 1,
-        speed:1000,
-        autoplay:{
-            delay:800,
-            disableOnInteraction: false,
-        },
-        // 슬라이드 이동 시 AOS 새로고침
-        on: {
-            slideChange: function () {
-                AOS.refresh();
-            }
-        }
-        
-    });    
-    
-    
-    const swiper_members = new Swiper('.section-calc .member-list', {
-        slidesPerView: 1.8,
-        spaceBetween: 12,
-        // 슬라이드 이동 시 AOS 새로고침
-        on: {
-            slideChange: function () {
-                AOS.refresh();
-            }
-        }
-    });
-
-    const swiper_review = new Swiper('.section-story .review', {
-        autoHeight: true,
-        slidesPerView: 1,
-        spaceBetween: 18,
-        pagination: {
-            el: ".section-story .review .swiper-pagination",
-            clickable: true,
-        },
-        // 슬라이드 이동 시 AOS 새로고침
-        on: {
-            slideChange: function () {
-                AOS.refresh();
-            }
-        }
-    });
-    
     AOS.init(); // AOS 초기화 코드
     var isVisible = false;
     // scroll event
@@ -97,15 +49,7 @@ $(document).ready(function(){
         $('.menu-toggle').removeClass('open');
         $('.header .nav').removeClass('open');
     });
-
-    // 탭 버튼에 대한 이벤트 리스너 추가
-    $('.tab-list .tablinks').click(function() {
-        var tabName = $(this).data('tab');
-        var tabGroup = $(this).closest('.tab-list').data('tab-group');
-        openTab(tabGroup, tabName);
-    });
 });
-
 
 // 요소가 화면에 보이는지 여부를 확인하는 함수
 function isElementInViewport(el) {
@@ -116,16 +60,6 @@ function isElementInViewport(el) {
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && // 요소의 하단이 뷰포트 내에 있고
         rect.right <= (window.innerWidth || document.documentElement.clientWidth) // 요소의 오른쪽이 뷰포트 내에 있으면
     );
-}
-
-// 탭을 열고 닫는 함수
-function openTab(tabGroup, tabName) {
-    $('#' + tabGroup + ' .tab-content').removeClass('current');
-    $('#' + tabName).addClass('current');
-    $('#' + tabGroup + ' .tablinks').removeClass('active');
-    $('#' + tabGroup + ' button[data-tab="' + tabName + '"]').addClass('active');
-    
-    AOS.refresh();
 }
 
 // 바텀시트 열고 닫는 함수
@@ -164,133 +98,7 @@ function stepNext() {
     // $('#' + sheetName + ' .sheet-wrap').addClass('animate-up');
 }
 
-$('.btn-step').click(function(e) {
-    e.preventDefault();
-    var parent = $(this).data('parent');
-    var tg = $(this).data('tg');
-    var amount = $(this).data('amount')
-
-    if (amount) {
-        let amount_txt = ''
-        let contract_amount_txt = ''
-
-        if (amount == '10만원미만'){
-            amount_txt = '50~70'
-        }else if (amount == '10~20만원'){
-            amount_txt = '120~180'
-        }else if (amount == '30만원이상'){
-            amount_txt = '250~300'
-        }else if (amount == '1시간미만'){
-            amount_txt = '50~70'
-            contract_amount_txt = '5~10'
-        }else if (amount == '1~2시간이상'){
-            amount_txt = '120~180'
-            contract_amount_txt = '10~20'
-        }else if (amount == '3시간이상'){
-            amount_txt = '190~220'
-            contract_amount_txt = '15~25'
-        }else if (amount == '6시간이상'){
-            amount_txt = '250~300'
-            contract_amount_txt = '20~30'
-        }
-        $('.money-num').text(amount_txt)
-        if (contract_amount_txt!=''){
-            $('#contract_amount').text(contract_amount_txt)
-        }
-    }
-
-    $('#' + parent ).hide().removeClass('open');
-    $('#' + tg ).show().addClass('open');
-    $('.back-button').addClass('active');
-    let back_list = $('.back-button').data('back')
-    back_list.push(parent)
-    $('.back-button').data('back', back_list);
-});
-
 $('.close-button').click(function(e) {
     e.preventDefault();
     $('#sheet-calc .calc-step-wrap').hide().removeClass('open');
-    $('#calc-main').css('display','flex');
-    $('.back-button').removeClass('active');
 });
-
-$('.back-button').click(function(e) {
-    e.preventDefault();
-    let back_list = $('.back-button').data('back');
-    let back = back_list.pop();
-    $('.back-button').data('back', back_list);
-
-    if (back == 'calc-main'){
-        $('.back-button').removeClass('active');
-    }
-
-    $('#sheet-calc .open').hide().removeClass('open');
-    $('#' + back ).show().addClass('open');
-});
-
-$(".privacy-check input[name='terms_chk']").change(function(){
-    const checked = $(this).val()
-    if (checked == 'true'){
-        $('#c_terms').prop("checked", true);
-        $('#sheet-agreement1 a').removeAttr("disabled")
-    }else{
-        $('#c_terms').prop("checked", false);
-        $('#sheet-agreement1 a').attr('disabled', 'disabled')
-    }
-});
-
-$(".privacy-check input[name='counsel_chk']").change(function(){
-    const checked = $(this).val()
-    if (checked == 'true'){
-        $('#c_counsel').prop("checked", true);
-        $('#sheet-agreement2 a').removeAttr("disabled")
-    }else{
-        $('#c_counsel').prop("checked", false);
-        $('#sheet-agreement2 a').attr('disabled', 'disabled')
-    }
-});
-
-$("#c_agree").click(function() {
-    if($("#c_agree").is(":checked")){
-        $("input[name=agree_chk]").prop("checked", true);
-        $("input:radio[name='terms_chk']:radio[value='true']").prop('checked', true);
-        $("input:radio[name='counsel_chk']:radio[value='true']").prop('checked', true);
-        $('#sheet-agreement1 a').removeAttr("disabled")
-        $('#sheet-agreement2 a').removeAttr("disabled")
-    }else{
-        $("input[name=agree_chk]").prop("checked", false);
-        $("input:radio[name='terms_chk']:radio[value='false']").prop('checked', true);
-        $("input:radio[name='counsel_chk']:radio[value='false']").prop('checked', true);
-        $('#sheet-agreement1 a').attr('disabled', 'disabled')
-        $('#sheet-agreement2 a').attr('disabled', 'disabled')
-    }
-});
-
-$("input[name=agree_chk]").click(function() {
-    const total = $("input[name=agree_chk]").length;
-    const total_checked = $("input[name=agree_chk]:checked").length;
-    const checked = $(this).is(':checked');
-    const val = $(this).val()
-
-    if(val == 'terms'){
-        if (checked){
-            $("input:radio[name='terms_chk']:radio[value='true']").prop('checked', true);
-            $('#sheet-agreement1 a').removeAttr("disabled")
-        }else{
-            $("input:radio[name='terms_chk']:radio[value='false']").prop('checked', true);
-            $('#sheet-agreement1 a').attr('disabled', 'disabled')
-        }
-    }else if(val == 'counsel'){
-        if (checked){
-            $("input:radio[name='counsel_chk']:radio[value='true']").prop('checked', true);
-            $('#sheet-agreement2 a').removeAttr("disabled")
-        }else{
-            $("input:radio[name='counsel_chk']:radio[value='false']").prop('checked', true);
-            $('#sheet-agreement2 a').attr('disabled', 'disabled')
-        }
-    }
-
-    if(total != total_checked) $("#c_agree").prop("checked", false);
-    else $("#c_agree").prop("checked", true); 
-});
-
